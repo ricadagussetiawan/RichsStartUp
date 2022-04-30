856e8b2c11a9222d6024f7a5e7a447e3ace644a4
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-04-30 21:23:52 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://d185-36-70-178-111.ngrok.io/bwastartup/campaign',
+    baseURL: 'https://d185-36-70-178-111.ngrok.io/bwastartup/campaign',
   },
 
   auth: {
