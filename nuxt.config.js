b751f854b1454089b97bc19e6efefbe3cda63c4c
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-05-01 03:47:56 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://localhost:8080',
+    baseURL: 'https://server108.herokuapp.com',
   },
 
   auth: {
