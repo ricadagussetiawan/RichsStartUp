bdc248350030e38fa9b7a321b8548c9130218c21
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-05-01 20:12:41 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,8 +50,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://ebc2988a.golang22.pages.dev
-',
+    baseURL: 'https://ebc2988a.golang22.pages.dev'
   },
 
   auth: {
